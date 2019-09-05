81d6e2be1046ba75eacb9a3338943875698ed16e
Bhavin <Bhavin@Bhavins-MacBook-Pro-2.local>
2019-09-04 22:07:05 -0400

    remove history

--- .history/dyng_project/blog/views_20190821030429.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return render(request, 'blog/home.html')
-
-def about(request):
-    return HttpResponse('<h1> About </h1>')
-# Create your views here
--- .history/dyng_project/blog/views_20190821030529.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return render(request, 'blog/home.html')
-
-def about(request):
-    return render(request, 'blog/about.html')
-# Create your views here
